ce664b0e77da2ed617c0980b9e8644185d126f8e
Sourcery AI <>
2023-06-23 09:55:03 +0000

    'Refactored by Sourcery'

--- jumpbug_printer.py
+++ jumpbug_printer.py
@@ -5,12 +5,21 @@ Use IDLE3 to copy the printed code.
 
 def jb(fps, waits):
     i = 2
-    script = 'alias jb1 "fps_max ' + str(fps) + ';w;+jump;w;-jump;alias @jb jb2;bxt_append _jb"\n'
-    for waits in range(0, waits):
-        script += 'alias jb' + str(i) + ' "w;alias @jb jb' + str(i+1) + ';bxt_append _jb"\n'
+    script = (
+        f'alias jb1 "fps_max {str(fps)}'
+        + ';w;+jump;w;-jump;alias @jb jb2;bxt_append _jb"\n'
+    )
+    for _ in range(0, waits):
+        script += (
+            f'alias jb{str(i)} "w;alias @jb jb{str(i + 1)}'
+            + ';bxt_append _jb"\n'
+        )
         i += 1
 
-    script += 'alias jb' + str(i) + ' "+duck;w 5;say jumpbug!;-duck;+jump;w;-jump;alias @jb"\n\n'
+    script += (
+        f'alias jb{str(i)}'
+        + ' "+duck;w 5;say jumpbug!;-duck;+jump;w;-jump;alias @jb"\n\n'
+    )
     script += 'alias +jumpbug "alias _jb @jb;jb1"\n'
     script += 'alias -jumpbug "alias _jb;fps_max 100"'
     print(script)
